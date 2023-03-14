51345f208730644d21de21de9683f5bc62e3c1b0
hyunwoongko <gusdnd852@naver.com>
2023-03-14 16:43:21 +0900

    [#3] Add training code baseline

---
+++ ahn/training/_decoder.py
@@ -0,0 +1,112 @@
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+import torch.distributed as dist
+from catalyst.data import DistributedSamplerWrapper
+from torch.utils.data import Dataset, DataLoader, DistributedSampler
+from tqdm import tqdm
+
+from _utils import SmartBatchSampler
+
+
+class DecoderDataset(Dataset):
+    def __init__(self, sentences, tokens):
+        self.sentences = sentences
+        self.tokens = tokens
+
+    def __len__(self):
+        return len(self.sentences)
+
+    def __getitem__(self, item):
+        sentences = self.sentences[item]
+        tokens = self.tokens[item]
+
+        return {
+            "sentences": sentences,
+            "tokens": tokens,
+        }
+
+
+def tokenize(config, tokenizer, data, data_type):
+    outputs = []
+    with ThreadPoolExecutor(max_workers=os.cpu_count() - 1) as pool:
+        imap = pool.map(
+            lambda x: tokenizer(
+                x,
+                padding=False,
+                truncation=False,
+                max_length=config["model_and_tokenizer"]["max_length"],
+                return_tensors="pt",
+                return_attention_mask=False,
+                return_token_type_ids=False,
+            ).input_ids.squeeze(),
+            data,
+        )
+
+        if dist.get_rank() == 0:
+            print(f"Tokenizing {data_type} data...")
+            imap = tqdm(imap, total=len(data))
+        for token in imap:
+            outputs.append(token)
+
+    return outputs
+
+
+def get_data_loader(config, tokenizer, data_type):
+    if data_type == "train":
+        data = open(config["data"]["train_data_path"]).read().splitlines()
+    elif data_type == "valid":
+        data = open(config["data"]["valid_data_path"]).read().splitlines()
+    else:
+        raise ValueError(f"Invalid data_type: {data_type}")
+
+    tokens = tokenize(config, tokenizer, data, data_type)
+    dataset = DecoderDataset(data, tokens)
+
+    def collate_fn(batch):
+        _tokens, _sentences = [], []
+        for sample in batch:
+            _tokens.append(sample["tokens"])
+            _sentences.append(sample["sentences"])
+
+        enc = tokenizer.pad(encoded_inputs={"input_ids": _tokens}, padding=True)
+        input_ids = enc.input_ids[:, : config["model_and_tokenizer"]["max_length"]]
+        attention_mask = enc.attention_mask[
+            :, : config["model_and_tokenizer"]["max_length"]
+        ]
+
+        outputs = {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "labels": input_ids,
+            "use_cache": False,
+        }
+
+        if (
+            "token_type_ids" in enc
+            and "token_type_ids" in config["model_and_tokenizer"]["model_input_names"]
+        ):
+            outputs["token_type_ids"] = enc.token_type_ids[
+                :, : config["model_and_tokenizer"]["max_length"]
+            ]
+
+        return outputs
+
+    if config["efficiency"]["smart_batching"]:
+        sampler = SmartBatchSampler(
+            dataset, batch_size=config["deepspeed"]["train_micro_batch_size_per_gpu"]
+        )
+        sampler = DistributedSamplerWrapper(sampler=sampler, shuffle=True)
+    else:
+        sampler = DistributedSampler(dataset=dataset, shuffle=True)
+
+    return DataLoader(
+        dataset,
+        batch_size=config["deepspeed"]["train_micro_batch_size_per_gpu"],
+        num_workers=os.cpu_count() // dist.get_world_size(),
+        shuffle=False,
+        drop_last=False,
+        pin_memory=True,
+        collate_fn=collate_fn,
+        sampler=sampler,
+    )
---
+++ ahn/training/_utils.py
@@ -0,0 +1,145 @@
+import functools
+import logging
+import os
+from itertools import chain
+
+import numpy as np
+import torch
+import torch.distributed as dist
+import transformers
+from torch.utils.data import Dataset, Sampler
+
+
+def rsetattr(obj, attr, val):
+    pre, _, post = attr.rpartition(".")
+    return setattr(rgetattr(obj, pre) if pre else obj, post, val)
+
+
+def rgetattr(obj, attr, *args):
+    def _getattr(obj, attr):
+        return getattr(obj, attr, *args)
+
+    return functools.reduce(_getattr, [obj] + attr.split("."))
+
+
+def default_setup():
+    os.environ["TOKENIZERS_PARALLELISM"] = "true"
+    logging.getLogger("transformers").setLevel(logging.ERROR)
+    dist.init_process_group(backend="nccl")
+    torch.cuda.set_device(dist.get_rank())
+
+
+def add_tokens(tokenizer, model, tokens):
+    tokenizer.add_tokens(tokens)
+    model.resize_token_embeddings(len(tokenizer))
+    return tokenizer, model
+
+
+def optimized_params(model, weight_decay):
+    no_decay = ["bias", "LayerNorm.weight"]
+
+    return [
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if not any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": weight_decay,
+        },
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.0,
+        },
+    ]
+
+
+class SmartBatchSampler(Sampler):
+    def __init__(
+        self,
+        data_source: Dataset,
+        batch_size: int,
+        preload_cache: bool = False,
+    ) -> None:
+        super(SmartBatchSampler, self).__init__(data_source=data_source)
+        self.batch_size = batch_size
+        self.data_source = data_source
+
+        sentence_lengths = [
+            len(items["tokens"]) if items["tokens"].dim() != 0 else 1
+            for items in data_source
+        ]
+        sentence_indices = [idx for idx in range(len(data_source))]
+
+        pack_by_length = list(zip(sentence_lengths, sentence_indices))
+        if preload_cache:
+            sort_by_length = sorted(pack_by_length, reverse=True)
+        else:
+            sort_by_length = sorted(pack_by_length)
+        sentence_lengths, sentence_indices = zip(*sort_by_length)
+
+        self.bins = [
+            sentence_indices[i : i + batch_size]
+            for i in range(0, len(sentence_indices), batch_size)
+        ]
+        self.bins = list(chain.from_iterable(self.bins))
+
+        if not preload_cache:
+            np.random.shuffle(self.bins)
+
+    def __iter__(self):
+        for ids in self.bins:
+            yield ids
+
+    def __len__(self):
+        return len(self.bins)
+
+
+def fuse_gelu_megatron(model):
+    @torch.jit.script
+    def gelu_fwd(x):
+        return x * 0.5 * (1.0 + torch.tanh(0.79788456 * x * (1 + 0.044715 * x * x)))
+
+    @torch.jit.script
+    def gelu_bwd(g, x):
+        tanh_out = torch.tanh(0.79788456 * x * (1 + 0.044715 * x * x))
+        ff = 0.5 * x * (
+            (1 - tanh_out * tanh_out) * (0.79788456 + 0.1070322243 * x * x)
+        ) + 0.5 * (1 + tanh_out)
+        return ff * g
+
+    class _FusedGeLUFunction(torch.autograd.Function):
+        @staticmethod
+        # bias is an optional argument
+        def forward(ctx, input):
+            ctx.input_tensor = input
+            return gelu_fwd(input)
+
+        @staticmethod
+        def backward(ctx, grad_output):
+            input = ctx.input_tensor
+            tmp = gelu_bwd(grad_output, input)
+            return tmp
+
+    class FusedGelu(torch.nn.Module):
+        def forward(self, input):
+            return _FusedGeLUFunction.apply(input)
+
+    fused_gelu_module = FusedGelu()
+    hf_gelu_functions = [
+        transformers.activations.GELUActivation,
+        transformers.activations.FastGELUActivation,
+        transformers.activations.NewGELUActivation,
+        transformers.activations.QuickGELUActivation,
+    ]
+
+    for name, module in model.named_modules():
+        for hf_gelu_function in hf_gelu_functions:
+            if isinstance(module, hf_gelu_function):
+                rsetattr(model, name, fused_gelu_module)
+
+    return model
